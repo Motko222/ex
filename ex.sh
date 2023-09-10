264040bfb9e8e18b34e8df31bf283a267c51f06b
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-10 13:40:59 +0200

    Create ex.sh

---
+++ ex.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z $1 ]
+  then
+    echo "------------------------"
+    ls ~/config | sed 's/.sh//g' | nl
+    echo "------------------------"
+    read -p "Repo? " repo
+
+    if [[ $repo == ?(-)+([0-9]) ]]
+      then
+        repo=$(ls ~/config | sed 's/.sh//g' | head -$repo | tail -1 )
+    fi
+
+  else
+    repo=$1
+fi
+
+if [ -z $2 ]
+  then
+    echo "------------------------"
+    ls ~/scripts/$repo | sed 's/.sh//g' | sed 's/.git//g' | nl
+    echo "------------------------"
+    read -p "Action? " action
+    echo "------------------------"
+    if [[ $action == ?(-)+([0-9]) ]]
+      then
+        action=$(ls ~/scripts/$repo | sed 's/.sh//g' | sed 's/.git//g' | head -$action | tail -1 )
+    fi
+
+  else
+    action=$2
+fi
+
+bash ~/scripts/$repo/$action.sh
