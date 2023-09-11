ad5509d193afb514a87bab44c14d140fd32d267e
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-11 10:55:08 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-if [ -z $1 ]
+echo "------------------------"
+ls ~/scripts | nl
+echo "------------------------"
+read -p "Repo? " repo
+
+if [[ $repo == ?(-)+([0-9]) ]]
   then
-    echo "------------------------"
-    ls ~/scripts | nl
-    echo "------------------------"
-    read -p "Repo? " repo
-    if [[ $repo == ?(-)+([0-9]) ]]
-      then
-        repo=$(ls ~/scripts | head -$repo | tail -1 )
-    fi
-    if [[ $repo == "all" ]]
-      then repo=$(ls ~/scripts)
-    fi
-  else
-    repo=$1
+    repo=$(ls ~/scripts | head -$repo | tail -1 )
+fi
+
+if [[ $repo == "all" ]]
+  then repo=$(ls ~/scripts)
 fi
 
 for i in $repo
@@ -24,4 +21,3 @@ do
 done  
 
 chmod +x -R ~/scripts
-
