65041cde7a7f3052b09c5807200139831e42c95b
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-20 11:34:53 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~
+
+if [ ! -d ~/scripts ] 
+  then
+    mkdir scripts
+fi
+
+if [ ! -d ~/logs ] 
+  then
+    mkdir logs
+fi
+
+cd scripts
+git clone https://github.com/Motko222/ex
+chmod +x ~/scripts/ex/*.sh
+echo alias ex="bash ~/scripts/ex/ex.sh" >> ~/.bashrc
+source ~/.bashrc
+echo "------------------------"
+echo "Simple script launcher installed. Run "ex" to open client.
