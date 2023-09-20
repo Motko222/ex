f7b9b29313aa717a8075824773c0e3956d0c947d
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-20 12:04:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 cd scripts
 git clone https://github.com/Motko222/ex
 chmod +x ~/scripts/ex/*.sh
-echo alias ex="bash ~/scripts/ex/ex.sh" >> ~/.bashrc
-source ~/.bashrc
+echo alias ex=\"bash ~/scripts/ex/ex.sh\" >> ~/.bashrc
+alias ex="bash ~/scripts/ex/ex.sh"
 echo "------------------------"
 echo "Simple script launcher installed. Run 'ex' to open client."
